242aab66b44957465c6341f13273d571baf53b1f
LeonelLemus <llemusm8@miumg.edu.gt>
2024-06-21 20:15:03 -0600

    Catalogo canciones terminado

--- Aplicacion de Musica/frmCatalogo.Designer.cs
+++ Aplicacion de Musica/frmCatalogo.Designer.cs
@@ -28,20 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCatalogo));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listViewCatalogo = new System.Windows.Forms.ListView();
+            this.btnReproducir = new System.Windows.Forms.Button();
+            this.btnPausarReanudar = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 678);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(266, 51);
+            this.panel2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(122, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Playlist";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Crear una PLaylist";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ver mis playlist";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listViewCatalogo
+            // 
+            this.listViewCatalogo.AllowDrop = true;
+            this.listViewCatalogo.HideSelection = false;
+            this.listViewCatalogo.LargeImageList = this.imageList;
+            this.listViewCatalogo.Location = new System.Drawing.Point(281, 14);
+            this.listViewCatalogo.Name = "listViewCatalogo";
+            this.listViewCatalogo.Size = new System.Drawing.Size(1080, 515);
+            this.listViewCatalogo.SmallImageList = this.imageList;
+            this.listViewCatalogo.StateImageList = this.imageList;
+            this.listViewCatalogo.TabIndex = 1;
+            this.listViewCatalogo.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnReproducir
+            // 
+            this.btnReproducir.Location = new System.Drawing.Point(338, 568);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(140, 59);
+            this.btnReproducir.TabIndex = 2;
+            this.btnReproducir.Text = "Reproducir";
+            this.btnReproducir.UseVisualStyleBackColor = true;
+            this.btnReproducir.Click += new System.EventHandler(this.btnReproducir_Click);
+            // 
+            // btnPausarReanudar
+            // 
+            this.btnPausarReanudar.Location = new System.Drawing.Point(535, 568);
+            this.btnPausarReanudar.Name = "btnPausarReanudar";
+            this.btnPausarReanudar.Size = new System.Drawing.Size(149, 59);
+            this.btnPausarReanudar.TabIndex = 3;
+            this.btnPausarReanudar.Text = "Pausar";
+            this.btnPausarReanudar.UseVisualStyleBackColor = true;
+            this.btnPausarReanudar.Click += new System.EventHandler(this.btnPausarReanudar_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(80, 80);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(838, 568);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(68, 24);
+            this.lblEstado.TabIndex = 4;
+            this.lblEstado.Text = "Estado";
             // 
             // frmCatalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1387, 678);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnPausarReanudar);
+            this.Controls.Add(this.btnReproducir);
+            this.Controls.Add(this.listViewCatalogo);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "frmCatalogo";
-            this.Text = "frmCatalogo";
+            this.Text = "Music-App";
             this.Load += new System.EventHandler(this.frmCatalogo_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewCatalogo;
+        private System.Windows.Forms.Button btnReproducir;
+        private System.Windows.Forms.Button btnPausarReanudar;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Label lblEstado;
     }
 }
--- Aplicacion de Musica/frmCatalogo.cs
+++ Aplicacion de Musica/frmCatalogo.cs
@@ -1,25 +1,202 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Text;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using NAudio.Wave;
 
 namespace Aplicacion_de_Musica
 {
     public partial class frmCatalogo : Form
     {
+        private IWavePlayer waveOutDevice;
+        private AudioFileReader audioFileReader;
+        private bool isPaused;
+
+
         public frmCatalogo()
         {
             InitializeComponent();
+
+            listViewCatalogo.View = View.Details;
+            listViewCatalogo.OwnerDraw = true;
+            listViewCatalogo.Columns.Add("Canciones", 250);
+            listViewCatalogo.Columns.Add("Álbum", 200);
+            listViewCatalogo.Columns.Add("Artista(s)", 200);
+            listViewCatalogo.FullRowSelect = true;
+            listViewCatalogo.GridLines = false;
+            listViewCatalogo.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+
+            listViewCatalogo.DrawColumnHeader += listViewCatalogo_DrawColumnHeader;
+            listViewCatalogo.DrawItem += listViewCatalogo_DrawItem;
+            listViewCatalogo.DrawSubItem += listViewCatalogo_DrawSubItem;
+
+            btnPausarReanudar.Text = "Pausar";
         }
 
         private void frmCatalogo_Load(object sender, EventArgs e)
         {
 
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;Port=3306;Database=musicapp;user=root;password=;"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Canciones", con);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    ListViewItem item = new ListViewItem(row["Titulo"].ToString());
+                    item.SubItems.Add(row["Album"].ToString());
+                    item.SubItems.Add(row["Artista"].ToString());
+                    item.Tag = row["AudioFilePath"].ToString();
+
+                    string imagenRelativa = row["ImagenUrl"].ToString();
+                    string imagenAbsoluta = Path.Combine(baseDirectory, imagenRelativa);
+                    if (File.Exists(imagenAbsoluta))
+                    {
+                        try
+                        {
+                            using (FileStream stream = new FileStream(imagenAbsoluta, FileMode.Open))
+                            {
+                                Image img = Image.FromStream(stream);
+                                imageList.Images.Add(img);
+                                item.ImageIndex = imageList.Images.Count - 1;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Archivo de imagen no encontrado: " + imagenAbsoluta);
+                    }
+
+                    listViewCatalogo.Items.Add(item);
+                }
+            }
+        }
+
+        private void listViewCatalogo_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            using (SolidBrush brush = new SolidBrush(Color.LightGray))
+            {
+                e.Graphics.FillRectangle(brush, e.Bounds);
+            }
+            using (Pen pen = new Pen(Color.Gray))
+            {
+                e.Graphics.DrawRectangle(pen, e.Bounds);
+            }
+            TextRenderer.DrawText(e.Graphics, e.Header.Text, e.Font, e.Bounds, Color.Black, TextFormatFlags.VerticalCenter | TextFormatFlags.HorizontalCenter);
+        }
+
+        private void listViewCatalogo_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            e.DrawDefault = true;
+            if (e.Item.Selected)
+            {
+                using (Pen pen = new Pen(Color.Green, 5))
+                {
+                    Rectangle rect = e.Bounds;
+                    rect.Width -= 3;
+                    rect.Height -= 3;
+                    e.Graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+
+        private void listViewCatalogo_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            if (e.Item.Selected)
+            {
+                e.Graphics.FillRectangle(Brushes.LightGreen, e.Bounds);
+            }
+            else
+            {
+                e.DrawBackground();
+            }
+
+            e.DrawText();
+        }
+
+        private void ReproducirCancion(string filePath)
+        {
+            DetenerReproduccion();
+
+            waveOutDevice = new WaveOut();
+            audioFileReader = new AudioFileReader(filePath);
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+
+            waveOutDevice.PlaybackStopped += (s, a) =>
+            {
+                DetenerReproduccion();
+            };
+
+            lblEstado.Text = "Reproduciendo: " + Path.GetFileName(filePath);
+        }
+
+        private void DetenerReproduccion()
+        {
+            if (waveOutDevice != null)
+            {
+                waveOutDevice.Stop();
+                waveOutDevice.Dispose();
+                waveOutDevice = null;
+            }
+            if (audioFileReader != null)
+            {
+                audioFileReader.Dispose();
+                audioFileReader = null;
+            }
+
+            lblEstado.Text = "Reproducción detenida";
+        }
+
+        private void btnReproducir_Click(object sender, EventArgs e)
+        {
+            if (listViewCatalogo.SelectedItems.Count > 0)
+            {
+                string filePath = listViewCatalogo.SelectedItems[0].Tag.ToString();
+                ReproducirCancion(filePath);
+                btnPausarReanudar.Text = "Pausar";
+                isPaused = false;
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una canción para reproducir.");
+            }
+        }
+
+        private void btnPausarReanudar_Click(object sender, EventArgs e)
+        {
+            if (waveOutDevice != null)
+            {
+                if (isPaused)
+                {
+                    waveOutDevice.Play();
+                    btnPausarReanudar.Text = "Pausar";
+                    lblEstado.Text = "Reproducción reanudada";
+                    isPaused = false;
+                }
+                else
+                {
+                    waveOutDevice.Pause();
+                    btnPausarReanudar.Text = "Reanudar";
+                    lblEstado.Text = "Reproducción pausada";
+                    isPaused = true;
+                }
+            }
         }
     }
 }
+    
+
